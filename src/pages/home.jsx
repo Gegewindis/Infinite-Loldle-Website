08f29aff04b7f3f029f64650a99b764cef507694
Gegewindis <georgebobocea2005@gmail.com>
2026-02-16 20:06:52 +0100

    added a backend connect test

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,16 @@
 import Header from "../components/Header.jsx"
 import SelectGameMode from "../components/SelectGameMode.jsx"
+import { useEffect } from "react";
 
 function Home() {
+
+    useEffect(() => {
+    fetch("http://localhost:8000/api/test/")
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+    }, []);
+    
     return (
         <>
             <Header></Header>
